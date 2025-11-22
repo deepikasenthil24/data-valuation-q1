ddea0654a641e1af44b0642d8631efcd786f6410
deepikasenthil24 <146707113+deepikasenthil24@users.noreply.github.com>
2025-11-22 01:52:48 -0800

    added indices for exp_1

--- savage/script.py
+++ savage/script.py
@@ -67,7 +67,7 @@ top_results_auc = run_beam_search(
     pipeline,                           
     lambda X, y, y_pred: auc(X, y, y_pred), 
     budget,                         
-    error_type='MNAR',             
+    error_type='Label',             
     random_state=RANDOM_STATE,        
     top_k=top_k                    
 )
